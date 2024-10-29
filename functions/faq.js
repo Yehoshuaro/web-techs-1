c9269bfa0e450e6e5c86efd5d1fd0cff6a24cd64
Ернар <230709@astanait.edu.kz>
2024-10-29 16:14:53 +0500

    /

--- functions/faq.js
+++ functions/faq.js
@@ -1,6 +1,5 @@
 document.getElementById('darkModeToggle').addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
 
-    // Toggle button text between "Dark Mode" and "Light Mode"
     this.textContent = document.body.classList.contains('dark-mode') ? 'Light Mode' : 'Dark Mode';
 });
